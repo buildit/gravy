2aaa8c383bb52bed3869fd639437a35934f34115
Matteo Pescarin <matteo.pescarin@wipro.com>
2018-01-16 22:42:06 +0000

    added custom typefaces test suite
    - updated mocha test runner

--- test/test_sass.js
+++ test/test_sass.js
@@ -1,5 +1,8 @@
-var path = require('path');
-var sassTrue = require('sass-true');
+const path = require('path');
+const sassTrue = require('sass-true');
 
-var sassFile = path.join(__dirname, 'init.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
+const init = path.join(__dirname, 'init.scss');
+sassTrue.runSass({file: init}, describe, it);
+
+// const initCustomTypefaces = path.join(__dirname, 'init-custom-typefaces.scss');
+// sassTrue.runSass({file: initCustomTypefaces}, describe, it);
