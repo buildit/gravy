13e0df8acbeabbd8b21cfd62ca19e20c949c2e44
Matteo Pescarin <matteo.pescarin@wipro.com>
2018-01-17 09:45:01 +0000

    added test for custom with typefaces
    - renamed no config test file
    - created new test suite

--- test/test_sass.js
+++ test/test_sass.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const sassTrue = require('sass-true');
 
-const init = path.join(__dirname, 'init.scss');
-sassTrue.runSass({file: init}, describe, it);
+const initNoCustomConfig = path.join(__dirname, 'init-no-custom-config.scss');
+sassTrue.runSass({file: initNoCustomConfig}, describe, it);
 
-// const initCustomTypefaces = path.join(__dirname, 'init-custom-typefaces.scss');
-// sassTrue.runSass({file: initCustomTypefaces}, describe, it);
+const initCustomTypefaces = path.join(__dirname, 'init-custom-typefaces.scss');
+sassTrue.runSass({file: initCustomTypefaces}, describe, it);
