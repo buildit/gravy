e5d74cb4f1fea9a7a89b5f8f024ded969a4c07a4
Matteo 'Peach' Pescarin <peach@smartart.it>
2019-07-07 12:03:41 +0200

    updated deps and fixed vulnerabilities

--- test/test_sass.js
+++ test/test_sass.js
@@ -2,10 +2,10 @@ const path = require('path');
 const sassTrue = require('sass-true');
 
 const initNoCustomConfig = path.join(__dirname, 'init-no-custom-config.scss');
-sassTrue.runSass({file: initNoCustomConfig}, describe, it);
+sassTrue.runSass({file: initNoCustomConfig}, { describe, it });
 
 const initCustomTypefaces = path.join(__dirname, 'init-custom-typefaces.scss');
-sassTrue.runSass({file: initCustomTypefaces}, describe, it);
+sassTrue.runSass({file: initCustomTypefaces}, { describe, it });
 
 const initCustomBreakpoints = path.join(__dirname, 'init-custom-breakpoints.scss');
-sassTrue.runSass({file: initCustomBreakpoints}, describe, it);
+sassTrue.runSass({file: initCustomBreakpoints}, { describe, it });
